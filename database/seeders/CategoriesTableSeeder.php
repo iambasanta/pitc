25a40bf96fa3c848811919464789843a5eaa05fb
iambasanta <sangpangbasanta@gmail.com>
2021-12-16 17:20:52 +0545

    Seed admin user and category

---
+++ database/seeders/CategoriesTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->truncate();
+
+        DB::table('categories')->insert([
+            [
+                'title' => 'Uncategorized',
+                'slug' => 'uncategorized'
+            ],
+        ]);
+    }
+}
