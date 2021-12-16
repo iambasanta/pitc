e6e8ec7afbd43282a07bd792ea7d4ab59f5e8f17
iambasanta <sangpangbasanta@gmail.com>
2021-12-16 20:34:53 +0545

    Show delete confirmation dialogue

--- resources/views/admin/members/index.blade.php
+++ resources/views/admin/members/index.blade.php
@@ -55,10 +55,10 @@
                             <tr>
                                 <td>
                                     <a href="{{route('members.edit',$member->id)}}" class="p-2 text-success"><i class="bi bi-pencil-square"></i></a>
-                                    <a href="#" class="p-2 text-danger" onclick="event.preventDefault();document.getElementById('delete-member-form').submit();">
+                                    <a href="#" class="p-2 text-danger" onclick="event.preventDefault();alert('Are you sure ?');document.getElementById('delete-form-{{$member->id}}').submit();">
                                         <i class="bi bi-trash"></i>
                                     </a>
-                                    <form id="delete-member-form" action="{{route('members.destroy',$member->id)}}" method="POST" class="d-none">
+                                    <form id="delete-form-{{$member->id}}" action="{{route('members.destroy',$member->id)}}" method="POST" class="d-none">
                                         @csrf
                                         @method('DELETE')
                                     </form>
